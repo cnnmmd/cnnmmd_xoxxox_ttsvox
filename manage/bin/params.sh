c9fd98d04eca08ab1eab3c0a35847037d31f966a
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-27 22:30:03 +0900

    migrate from local repo

---
+++ manage/bin/params.sh
@@ -0,0 +1,5 @@
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envvox.txt
+cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_ttsvox.yml
+pthdoc="${pthtop}"/export/cnf/xoxxox/docker
+imgtgt='xoxxox_envvox:001'
+cnttgt='xoxxox_ttsvox'
