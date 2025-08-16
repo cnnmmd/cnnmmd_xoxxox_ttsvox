126309a0bceb1ad86b84b06bbcf53792b1d8955c
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-16 23:32:08 +0900

    wip

--- manage/bin/params.sh
+++ manage/bin/params.sh
@@ -1,5 +1,5 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envvox
 cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envvox.txt
 cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_ttsvox.yml
-pthdoc="${pthtop}"/export/cnf/xoxxox/docker
-imgtgt='xoxxox_envvox:001'
+imgtgt='xoxxox/envvox:001'
 cnttgt='xoxxox_ttsvox'
