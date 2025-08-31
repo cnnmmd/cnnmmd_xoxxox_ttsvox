bafb5c5065d4248e4b347b05e9fb98f185c20e24
azsgit <10238943+azsgit@users.noreply.github.com>
2025-09-01 02:18:47 +0900

    wip

--- manage/bin/create.sh
+++ manage/bin/create.sh
@@ -9,25 +9,27 @@ pthapp="${pthsrc}"/appvox
 
 addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
 test -d "${pthapp}" || mkdir "${pthapp}"
-cd "${pthapp}"
-if test ! -e download
+if cd "${pthapp}"
 then
-  arc=$(docker run --rm ${imgtgt} uname -m)
-  if test ${arc} = 'x86_64'
+  if test ! -e download
   then
-    curl -sSfL https://github.com/VOICEVOX/voicevox_core/releases/latest/download/download-linux-x64 -o download && chmod 755 download
+    arc=$(docker run --rm ${imgtgt} uname -m)
+    if test ${arc} = 'x86_64'
+    then
+      curl -sSfL https://github.com/VOICEVOX/voicevox_core/releases/latest/download/download-linux-x64 -o download && chmod 755 download
+    fi
+    if test ${arc} = 'aarch64'
+    then
+      curl -sSfL https://github.com/VOICEVOX/voicevox_core/releases/latest/download/download-linux-arm64 -o download && chmod 755 download
+    fi
   fi
-  if test ${arc} = 'aarch64'
+  if test ! -d "${pthapp}"/voicevox_core
   then
-    curl -sSfL https://github.com/VOICEVOX/voicevox_core/releases/latest/download/download-linux-arm64 -o download && chmod 755 download
-  fi
-fi
-if test ! -d "${pthapp}"/voicevox_core
-then
-  if test -t 0
-  then
-    docker run -it --rm -v "${pthapp}":/opt/appvox --name ${cnttgt} ${imgtgt} bash -c "cd /opt/appvox && ./download"
-  else
-    docker run -i  --rm -v "${pthapp}":/opt/appvox --name ${cnttgt} ${imgtgt} bash -c "cd /opt/appvox && ./download"
+    if test -t 0
+    then
+      docker run -it --rm -v "${pthapp}":/opt/appvox --name ${cnttgt} ${imgtgt} bash -c "cd /opt/appvox && ./download"
+    else
+      docker run -i  --rm -v "${pthapp}":/opt/appvox --name ${cnttgt} ${imgtgt} bash -c "cd /opt/appvox && ./download"
+    fi
   fi
 fi
